94cde88127a7050337353c439f164dfd32a7d8b8
Oswin <oswinrayvijayaraj.r@contus.in>
2023-09-27 15:23:25 +0530

    animation screen with network checking

--- app/src/main/java/com/example/apicalltask/SplashScreenActivity.kt
+++ app/src/main/java/com/example/apicalltask/SplashScreenActivity.kt
@@ -8,6 +8,7 @@ import android.os.Handler
 import android.os.Looper
 import android.provider.SyncStateContract
 import android.view.WindowManager
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
@@ -21,25 +22,50 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
-        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
-        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
-        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
-        backgroundImage.startAnimation(slideAnimation)
+        // Load the fade-in animation from the XML resource.
+        val fadeAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler().postDelayed({
-            val intent = Intent(this, HomeScreenActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        // Find the ImageView or other views you want to animate.
+        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
+
+        // Set the animation to the view.
+        backgroundImage.startAnimation(fadeAnimation)
+
+        // Check network availability and proceed accordingly.
+        if (isNetworkAvailable()) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, HomeScreenActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 1000) // Delay for 1 second (1000 milliseconds)
+        } else {
+            showNoNetworkError()
+        }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+
+    private fun showNoNetworkError() {
+        // Display a message to the user or show a dialog
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("No Network Connection")
+        builder.setMessage("Please check your network connection and try again.")
+        builder.setPositiveButton("Retry") { _, _ ->
+            recreate() // Restart the activity to check for network again
+        }
+        builder.setNegativeButton("Exit") { _, _ ->
+            finish() // Exit the app if the user chooses to
+        }
+        val dialog = builder.create()
+        dialog.show()// 3000 is the delayed time in milliseconds.
     }
 }
